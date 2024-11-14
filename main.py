4ba5adf831b3e6377449b0234edb4dd69eb270f2
Thanh Duong <thanhduonghd114@gmail.com>
2024-11-14 15:03:51 +0200

    finalize

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import pickle
 
+# Load model 
 reg = pickle.load(open('model.pkl', 'rb'))
 
-# Visualizing the data
+# Input data
 hours_studied = input("Enter the number of hours studied: (0 -> 24) ")
 previous_scores = input("Enter the previous scores: (0 -> 100) ")
 extracurricular_activities = input("Enter the number of extracurricular activities: (0 for no and 1 for yes) ")
 sleep_hours = input("Enter the number of sleep hours: (0 -> 24) ")
 sample_question_papers_practiced = input("Enter the number of sample question papers practiced: ")
 
+# create a new input data
 new_data = {'Hours Studied': hours_studied, 'Previous Scores': previous_scores, 'Extracurricular Activities': extracurricular_activities ,'Sleep Hours': sleep_hours ,'Sample Question Papers Practiced': sample_question_papers_practiced}
 new_df = pd.DataFrame([new_data])
 
+
+# Predict data
 performance = reg.predict(new_df)
-print(performance)
-
-# new_df = pd.DataFrame([new_data])
-# performance = reg.predict(new_df)
-
-# print(performance)
+print("Student performance ", performance)
--- training_model.py
+++ training_model.py
@@ -14,23 +14,28 @@ import pickle
 
 data = pd.read_csv('/Users/thanhduong/Workspace/Predict Student Score/Student_Performance.csv')
 
+
 # Data cleaning
 
-x = data.iloc[:, :-1]
+x = data.iloc[:, :-1]               # Get the input data
 
-y = data.iloc[:,[-1]]
+y = data.iloc[:,[-1]]               # Get the label data
 
-
+# Because in Extracurricular Activities column, we have "Yes" and "No" (string data format) which is not used to train model
+# So, I have to convert it to 0 and 1 meaning that 0 is false and 1 is true 
 le = LabelEncoder()
 x['Extracurricular Activities'] = le.fit_transform(x['Extracurricular Activities']).astype(int)
-print(x.iloc[1:5,:-1])
 
+# get the train and test data for x and y
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 10)
-print(len(x_train) ," ",len(x_test))
 
+
+# Train model
 reg = LinearRegression()
 reg.fit(x_train,y_train)
+
 y_pred = reg.predict(x_test)
 result = np.concatenate((y_test[0:5],y_pred[0:5]), axis=1).round()
 
+# Create a model 
 pickle.dump(reg, open('model.pkl', 'wb'))
